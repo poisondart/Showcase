11ba708e2b56852c483806c1ca54b8b9c1ea40b2
Nikita Karpunin <nikita.karpunin@iss.digital>
2020-12-27 11:37:16 +0300

    Первый коммит

---
+++ app/src/main/java/com/example/gyrotest/GameView.kt
@@ -0,0 +1,53 @@
+package com.example.gyrotest
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.SurfaceView
+
+@SuppressLint("ViewConstructor")
+class GameView(context: Context, private val screenWidth: Int, private val screenHeight: Int): SurfaceView(context), Runnable {
+
+    @Volatile
+    private var playing = false
+    private var gameThread: Thread? = null
+
+    private val paint: Paint = Paint()
+    private var canvas: Canvas? = null
+
+    override fun run() {
+        while (playing) {
+            update()
+            draw()
+        }
+    }
+
+    private fun update() {
+
+    }
+
+    private fun draw() {
+        if (holder.surface.isValid) {
+            canvas = holder.lockCanvas()
+            canvas?.drawColor(Color.argb(255, 196, 240, 194))
+            holder.unlockCanvasAndPost(canvas)
+        }
+    }
+
+    fun pause() {
+        playing = false
+        try {
+            gameThread?.join()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun resume() {
+        playing = true
+        gameThread = Thread(this)
+        gameThread?.start()
+    }
+}
---
+++ app/src/main/java/com/example/gyrotest/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.gyrotest
+
+import android.graphics.Point
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var gameView: GameView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        gameView = GameView(this, size.x, size.y)
+        setContentView(gameView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        gameView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gameView.resume()
+    }
+}
